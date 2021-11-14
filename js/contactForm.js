eb021c9e7a00c36b2f57f2d337f450c981a6975f
Junnel Torate <jttorate@gmail.com>
2021-11-14 18:55:57 +0800

    ***

--- js/contactForm.js
+++ js/contactForm.js
@@ -7,5 +7,34 @@ const $contactMeFormButton = $contactMeForm.querySelector('button');
 /** Contact Me Form Submit */
 $contactMeForm.addEventListener('submit', (e) => {
 	e.preventDefault();
-	console.log('Submit');
+	showLoaderModal();
+
+	/** Disable Button */
+	$contactMeFormButton.setAttribute('disabled', 'disabled');
+
+	contactUsForm(
+		{
+			firstName: e.target.elements.firstName.value,
+			lastName: e.target.elements.lastName.value,
+			email: e.target.elements.email.value,
+			contactNumber: e.target.elements.contactNumber.value,
+			message: e.target.elements.message.value,
+		},
+		(error, data) => {
+			$contactMeFormButton.removeAttribute('disabled');
+			setTimeout(() => {
+				hideLoaderModal();
+
+				if (!error) {
+					/** Enable Form */
+					for (var i = 0; i < $contactMeFormInput.length; i++) {
+						$contactMeFormInput[i].value = '';
+					}
+					$contactMeFormTextarea.value = '';
+				}
+			}, 5000);
+
+
+		}
+	);
 });
